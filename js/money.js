ac369f638be93b38ea34251e1abdf9243556cf92
Fazle Rabbi <you@example.com>
2022-02-17 23:28:54 +0600

    almost done

--- js/money.js
+++ js/money.js
@@ -12,21 +12,44 @@ document.getElementById('calculate-button').addEventListener('click', function (
     const rentCost = getInput('rent-input');
     const clothesCost = getInput('clothes-input');
 
-    // calculate expenses
-    const newTotalExpenses = foodCost + rentCost + clothesCost;
+    if (foodCost >= 0 && rentCost >= 0 && clothesCost >= 0) {
+        // calculate expenses
+        const newTotalExpenses = foodCost + rentCost + clothesCost;
 
-    const totalExpenses = document.getElementById('total-expenses');
-    totalExpenses.innerText = newTotalExpenses;
+        const totalExpenses = document.getElementById('total-expenses');
+        totalExpenses.innerText = newTotalExpenses;
 
-    console.log(newTotalExpenses);
+        console.log(newTotalExpenses);
 
-    // calculate balance
-    const newBalance = incomeAmount - newTotalExpenses;
+        if (incomeAmount > newTotalExpenses) {
+            // calculate balance
+            const newBalance = incomeAmount - newTotalExpenses;
 
-    const balance = document.getElementById('balance');
-    balance.innerText = newBalance;
+            const balance = document.getElementById('balance');
+            balance.innerText = newBalance;
 
-    console.log(newBalance);
+            console.log(newBalance);
+        }
+        else if (incomeAmount == newTotalExpenses) {
+            const balance = document.getElementById('balance');
+            balance.innerText = '00';
+        }
+        else if (incomeAmount < newTotalExpenses) {
+            const balance = document.getElementById('balance');
+            balance.innerText = 'Income must be bigger than total expenses!';
+        }
+        else {
+            const balance = document.getElementById('balance');
+            balance.innerText = '';
+        }
+    }
+    else {
+        const totalExpenses = document.getElementById('total-expenses');
+        totalExpenses.innerText = 'Please fill all fields with positive numbers!';
+
+        const balance = document.getElementById('balance');
+        balance.innerText = '';
+    }
 })
 
 // handling save button
@@ -38,19 +61,42 @@ document.getElementById('save-button').addEventListener('click', function () {
     const balanceText = balance.innerText;
     const balanceAmount = parseFloat(balanceText);
 
-    // calculate saving amount
-    const newSavingAmount = incomeAmount * savePercentage / 100;
+    if (incomeAmount > 0) {
+        // calculate saving amount
+        const newSavingAmount = incomeAmount * savePercentage / 100;
 
-    const savingAmount = document.getElementById('saving-amount');
-    savingAmount.innerText = newSavingAmount;
+        const savingAmount = document.getElementById('saving-amount');
+        savingAmount.innerText = newSavingAmount;
 
-    console.log(newSavingAmount);
+        console.log(newSavingAmount);
 
-    // calculate remaining balance
-    const newRemainingBalance = balanceAmount - newSavingAmount;
+        if (balanceAmount > newSavingAmount) {
+            // calculate remaining balance
+            const newRemainingBalance = balanceAmount - newSavingAmount;
 
-    const remainingBalance = document.getElementById('remaining-balance');
-    remainingBalance.innerText = newRemainingBalance;
+            const remainingBalance = document.getElementById('remaining-balance');
+            remainingBalance.innerText = newRemainingBalance;
 
-    console.log(newRemainingBalance);
+            console.log(newRemainingBalance);
+        }
+        else if (balanceAmount == newSavingAmount) {
+            const remainingBalance = document.getElementById('remaining-balance');
+            remainingBalance.innerText = '00';
+        }
+        else if (balanceAmount < newSavingAmount) {
+            const remainingBalance = document.getElementById('remaining-balance');
+            remainingBalance.innerText = 'Balance must be bigger than saving amount!';
+        }
+        else {
+            const remainingBalance = document.getElementById('remaining-balance');
+            remainingBalance.innerText = '';
+        }
+    }
+    else {
+        const savingAmount = document.getElementById('saving-amount');
+        savingAmount.innerText = 'Please give income amount!';
+
+        const remainingBalance = document.getElementById('remaining-balance');
+        remainingBalance.innerText = '';
+    }
 })
